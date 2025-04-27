5f973a25ad2f6d5f8381bf1ab73ae6d72e83f452
NourKasmi <nourkasmi@gmail.com>
2025-04-27 18:05:34 +0100

    task02

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for flooring a float to an integer."""
+
+import math
+
+def floor(n: float) -> int:
+    """Return the floor of a floating point number."""
+    return math.floor(n)
