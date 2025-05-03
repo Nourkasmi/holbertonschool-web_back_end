2685080c00492007e25a133255cd9e759333538f
NourKasmi <nourkasmi@gmail.com>
2025-05-03 11:08:48 +0100

    task0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+async def async_generator():
+    """Yield a random float between 0 and 10, 10 times, with 1 second delay."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
