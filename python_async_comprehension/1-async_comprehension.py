2ae6355f1a4c20bfbca927ba9a07aab8b9b72511
NourKasmi <nourkasmi@gmail.com>
2025-05-03 11:58:07 +0100

    fix: update import statement and improve documentation

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-"""This module defines an async comprehension that collects random numbers from an async generator."""
+"""This module defines an async comprehension that collects random numbers
+from an async generator."""
 
 from typing import List
-from async_generator import async_generator
+
+
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
