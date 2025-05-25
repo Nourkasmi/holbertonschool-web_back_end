e5cc6e84ae59cd8bdb94b2c2b64d7c343bc9f0b7
NourKasmi <nourkasmi@gmail.com>
2025-05-25 23:29:14 +0100

    again

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,20 +1,60 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n');
+      lines.shift();
+
+      const studentsByField = {};
+      let totalStudents = 0;
+
+      for (const line of lines) {
+        if (line.trim() !== '') {
+          const [firstname, , , field] = line.split(',');
+
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+
+          studentsByField[field].push(firstname);
+          totalStudents += 1;
+        }
+      }
+
+      const linesToPrint = [];
+      linesToPrint.push(`Number of students: ${totalStudents}`);
+      for (const [field, students] of Object.entries(studentsByField)) {
+        linesToPrint.push(
+          `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
+        );
+      }
+
+      resolve(linesToPrint.join('\n'));
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
-  const { url } = req;
-
-  if (url === '/') {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (url === '/students') {
+  } else if (req.url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
+
     countStudents(process.argv[2])
-      .then(() => {
-        res.end();
+      .then((output) => {
+        res.end(`${output}\n`);
       })
       .catch(() => {
+        res.statusCode = 500;
         res.end('Cannot load the database');
       });
   } else {
